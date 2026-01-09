c7e0e65f282ca4600aeea08119b78e3a5b038b8f
enver.yildirim <enver.yildirim@aurorabilisim.com>
2026-01-09 11:08:41 +0300

    implemented task 0.1

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -114,7 +114,7 @@ def inv(x: float) -> float:
 
 def inv_back(x: float, d: float) -> float:
     r"If $f(x) = 1/x$ compute $d \times f'(x)$"
-    # TODO: Implement for Task 0.1.
+    # TODO: Implement for Task 0.1.K
     # raise NotImplementedError('Need to implement for Task 0.1')
     return -d / (x * x)
 
